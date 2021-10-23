54ce465c9d1232143d1d145264ad6f8d049b0dbd
gaopengtao <gaopengtao@myai.tech>
2021-10-23 22:37:04 +0800

    wwwwwwwwww

--- src/main/java/com/example/mywork/controller/UserController.java
+++ src/main/java/com/example/mywork/controller/UserController.java
@@ -17,6 +17,6 @@ public class UserController {
 
     @RequestMapping("getUser/{id}")
     public String GetUser(@PathVariable String id) {
-        return userService.Sel(id).toString() + "qqqqqqqqq";
+        return userService.Sel(id).toString() + "wwwwwwww";
     }
 }
