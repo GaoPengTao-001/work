ce66b0ea470d1b616cc8f941b0bc0377212553cd
gaopengtao <gaopengtao@myai.tech>
2021-12-02 16:10:29 +0800

    mq

--- src/main/java/com/example/mywork/service/DirectReceiver.java
+++ src/main/java/com/example/mywork/service/DirectReceiver.java
@@ -4,15 +4,13 @@ import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-
 @Component
 //监听的队列名称
 @RabbitListener(queues = "python-test")
 public class DirectReceiver {
 
     @RabbitHandler
-    public void process(Map testMessage) {
+    public void process(Object testMessage) {
         System.out.println("DirectReceiver消费者收到消息1  : " + testMessage.toString());
     }
 
